804f2541752687fef9b799e06ec08dd3664f15b8
Mars Getsoian <mars.getsoian@pcc.edu>
2024-05-13 14:54:10 -0700

    Class and sayHello() method

---
+++ JavaHelloWorld.java
@@ -0,0 +1,21 @@
+
+/**
+ * A Hello World demo
+ *
+ * @author Mars Getsoian
+ * @version 0.1 2024/5/11
+ */
+
+public class HelloWorld
+{
+
+    /**
+     * Print a hello world message
+     */
+    public void sayHello()
+    {
+        System.out.println("Hello, World!");
+    }
+
+}
+
